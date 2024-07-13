68777753315bee6137028614d1a1e7e42fb03721
Bryan Ribo <ribobryan@gmail.com>
2024-07-13 20:49:33 +0800

    feat: about us page

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,16 +2,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@pages/Home/Home";
-import AboutUs from "@pages/AboutUs/AboutUs";
+import WhoWhyCorsaFutures from "@pages/AboutUs/WhoWhyCorsaFutures";
 import RegulatorySupervision from "@pages/AboutUs/RegulatorySupervision";
+import LegalDocuments from "@pages/AboutUs/LegalDocuments";
+import PrivacyPolicy from "@pages/AboutUs/PrivacyPolicy";
+import ContactUs from "@pages/AboutUs/ContactUs";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/regulatory-supervision" element={<RegulatorySupervision />} />
+        <Route path="/about-us" element={<WhoWhyCorsaFutures />} />
+        <Route
+          path="/regulatory-supervision"
+          element={<RegulatorySupervision />}
+        />
+        <Route path="/legal-documents" element={<LegalDocuments />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/contact-us" element={<ContactUs />} />
         {/* <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </BrowserRouter>
