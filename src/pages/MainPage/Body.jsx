af90f7c545f6a978e59771798a9f19c1423c98d7
letternine24 <cj.natividad24@gmail.com>
2024-07-19 01:23:10 +0800

    fix main page

--- src/pages/MainPage/Body.jsx
+++ src/pages/MainPage/Body.jsx
@@ -1,24 +1,239 @@
-import '@styles/MainPage/Body.css'
-import GetNavigatingSection from './Components/NavigatingSection';
-import GetEmpoweringSection  from './Components/EmpoweringSection'
-import GetTradingSection from './Components/TradingSection';
-import GetDownloadSection from './Components/DownloadSection';
-import GetInstantSection from './Components/InstantSection';
-import GetSeizeSection from './Components/SeizeSection';
-import GetEmbarkSection from './Components/EmbarkSection';
+// import '@styles/MainPage/Body.css'
+// import NavigatingSection from './Components/NavigatingSection';
+// import GetEmpoweringSection  from './Components/EmpoweringSection'
+// import GetTradingSection from './Components/TradingSection';
+// import GetDownloadSection from './Components/DownloadSection';
+// import GetInstantSection from './Components/InstantSection';
+// import GetSeizeSection from './Components/SeizeSection';
+// import GetEmbarkSection from './Components/EmbarkSection';
 
-function Body() {
+// function Body() {
+//   return (
+//     <div id="corsa_body">
+//         <NavigatingSection />
+//         <GetEmpoweringSection />
+//         <GetTradingSection />
+//         <GetDownloadSection />
+//         <GetInstantSection />
+//         <GetSeizeSection />
+//         <GetEmbarkSection />
+//     </div>
+//   );
+// }
+
+// export default Body;
+
+import React from "react";
+import PageBody from "@components/Pages/PageBody";
+import '@styles/MainPage/Body.css'
+import DemoBtn from "../../components/Buttons/DemoTrading";
+
+const Body = () => {
+
+  // background images
+  const navigatingBg = "/assets/images/MainPage/navigating-bg.png";
+  const empoweringBg = "/assets/images/MainPage/empowering-bg.png";
+  const commonBg = "/assets/images/common-bg.png";
+  const seizeBg = "/assets/images/MainPage/seize-bg.png";
+  const embarkBg = "/assets/images/MainPage/embark-bg.png";
+  const yellowBg = "/assets/images/MainPage/yellow-bg.png";
+
+  // icons
+  const empoweringIC = "/assets/icons/MainPage/empowering-ic.png"
+  const tradingTextIC = 'assets/icons/MainPage/trading-text-ic.png'
+  const tradingImgIC = 'assets/icons/MainPage/trading-img-ic.png'
+  const downloadIC = 'assets/icons/MainPage/download-ic.png'
+  const paymentIC = 'assets/icons/MainPage/payment-ic.png'
+  const seizeIC = 'assets/icons/MainPage/seize-ic.png'
+
   return (
-    <div id="corsa_body">
-        <GetNavigatingSection />
-        <GetEmpoweringSection />
-        <GetTradingSection />
-        <GetDownloadSection />
-        <GetInstantSection />
-        <GetSeizeSection />
-        <GetEmbarkSection />
-    </div>
+    <>
+      <main>
+
+          {/* Navigating */}
+          <PageBody pageBgImg={navigatingBg}>
+ 
+            <div className="mainpage-body-container">
+              <div className="navigating-section">
+
+                <h1>
+                  <p>Navigating</p>
+                  <p>Opportunities,</p>
+                  <p>Mastering Markets:</p>
+                </h1>
+
+                <h2>Your Trusted Investment Partner</h2>
+
+                <DemoBtn />
+
+                <div className="navigating-section-risk">
+                  <p>
+                    Risk Warning:
+                  </p>
+                  <p>
+                    Trading involves risk. Please be aware of the risks involved and seek independent advice if necessary.
+                  </p>
+                </div>
+
+              </div>
+            </div>
+          </PageBody>
+          {/* Navigating */}
+
+          {/* Empowering */}
+          <PageBody pageBgImg={empoweringBg}>
+ 
+            <div className="empowering-section">
+
+              <h1>
+                <p>Empowering Your Financial Journey</p>
+                <p>with Precision and Performance.</p>
+              </h1>
+
+              <div className="empowering-icon-container">
+                <img src={empoweringIC}/>
+              </div>
+              
+            </div>
+          </PageBody>
+          {/* Empowering */}
+
+          {/* Trading */}
+          <PageBody pageBgImg={commonBg}>
+ 
+            <div className="trading-section">
+
+              <div className="main-page-hr"></div>
+              <h1>
+                <p>Trading Anywhere,</p>
+                <p>at Any Time</p>
+              </h1>
+
+              <div>
+                  <div className="trading-content-container">
+                      <div className="trading-content-left">
+                          
+                          <p>Trade Global Stocks, Forex, Metals, Crypto CFDs and More with our world recognized cTrader App.</p>
+
+                          <img src={tradingTextIC} />
+                      </div>
+
+                      <div className="trading-content-right">
+                          <img src={tradingImgIC} />
+                      </div>
+                  </div>
+                </div>
+              
+              
+            </div>
+          </PageBody>
+          {/* Trading */}
+
+          {/* Download */}
+          <PageBody pageBgImg="">
+ 
+            <div className="mainpage-download-section">
+
+              <div>
+                  <div className="mainpage-download-content-container">
+                      <div className="download-content-left">
+                          
+                      <h1>
+                        <p>Download Now</p>
+                      </h1>
+
+                      <h2>
+                        <p>Now you can download the app through</p>
+                        <p>Apple and Google Store</p>
+                      </h2>
+
+                      </div>
+
+                      <div className="download-content-right">
+                          <img src={downloadIC} />
+                      </div>
+                  </div>
+                </div>
+              
+              
+            </div>
+          </PageBody>
+          {/* Download */}
+
+          {/* Instant */}
+          <PageBody pageBgImg={yellowBg}>
+ 
+            <div className="mainpage-insant-section">
+
+              <div>
+                  <div className="mainpage-insant-content-container">
+                      <div className="instant-content-left">
+                        <div>
+                          <h1>
+                            <p>Instant Deposits,</p>
+                            <p>Fast withdrawals</p>
+                          </h1>
+
+                          <h2>
+                            <p>Fund your account easily and securely</p>
+                            <p>through our wide range of payment options</p>
+                          </h2>
+
+                          <h2>
+                            <p>Your funds are held securely in Tier 1 banks</p>
+                          </h2>
+                        </div>
+                      </div>
+
+                      <div className="instant-content-right">
+                          <img src={paymentIC} />
+                      </div>
+                  </div>
+                </div>
+            </div>
+          </PageBody>
+          {/* Instant */}
+
+          {/* Seize */}
+          <PageBody pageBgImg={seizeBg}>
+ 
+            <div className="seize-section">
+
+              <h1>Seize your trading destiny</h1>
+
+              <p>Join Today for Seamless Onboarding and Limitless Possibilities!</p>
+
+              <div className="seize-icon-container">
+                <img src={seizeIC}/>
+              </div>
+              
+            </div>
+          </PageBody>
+          {/* Seize */}
+
+          {/* Embark */}
+          <PageBody pageBgImg={embarkBg}>
+ 
+              <div className="embark-section">
+
+              <h1>
+                <p>Embark with Us on an</p>
+                <p>Exciting expedition</p>
+              </h1>
+
+              <p>Charting Success Together: Your Voyage Starts Here.</p>
+              <p>Get Started! Sign up and access the Global</p>
+              <p>Markets in less than 5 minutes.</p>
+
+              <DemoBtn />
+
+            </div>
+          </PageBody>
+          {/* Embark */}
+
+      </main>
+    </>
   );
-}
+};
 
 export default Body;
