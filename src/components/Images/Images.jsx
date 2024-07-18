33f38a4d4abdc4735b5aa00bbdd7f4f58f65a0f8
bryyyanribo <ribobryan@gmail.com>
2024-07-18 21:17:11 +0800

    Update

--- src/components/Images/Images.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "@styles/Components/Images.css";
-
-const Images = ({ name, size }) => {
-  return <img src={`/assets/images/${name}.png`} className={`images`} />;
-};
-
-export default Images;
