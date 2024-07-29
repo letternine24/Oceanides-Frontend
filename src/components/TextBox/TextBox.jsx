5e6258ff48e3ff8133e01eedc67e1e392e4875ff
letternine24 <cj.natividad24@gmail.com>
2024-07-29 22:19:53 +0800

    Sign-up initial

--- src/components/TextBox/TextBox.jsx
+++ src/components/TextBox/TextBox.jsx
@@ -1,80 +1,5 @@
-// import React, { useState } from 'react';
-// //import "@styles/Components/TextBox.css";
-
-// const TextBox = ({ id, regexType, isRequired }) => {
-//     const [isValid, setIsValid] = useState(true);
-//     const [value, setValue] = useState();
-  
-//     const handleChange = (event) => {
-//       const inputValue = event.target.value;
-
-//       let finalRegex;
-//       let errorMessage;
-//       const alphanumericRegex = /^[a-zA-Z0-9]*$/; //1
-//       const alphaRegex = /^[a-zA-Z]+$/; //2
-//       const numericRegex = /^[0-9]\d*$/; //3
-//       const emailRegex = /^[0-9]\d*$/; //4
-//       const noRegex = /.*/; //5
-
-//       const alphanumericErrorMsg = "Please enter only alphanumeric value";
-//       const alphaErrorMsg = "Please enter a valid value";
-//       const numericErrorMsg = "Please enter a valid number";
-//       const emailErrorMsg = "Please enter a valid email address"
-
-//       switch (regexType){
-//         case 1:
-//             finalRegex = alphanumericRegex;
-//             errorMessage = alphanumericErrorMsg;
-//             break;
-//         case 2: 
-//             finalRegex = alphaRegex;
-//             errorMessage = alphaErrorMsg;
-//             break;
-//         case 3:
-//             finalRegex = numericRegex;
-//             errorMessage = numericErrorMsg;
-//             break;
-//         case 4:
-//             finalRegex = emailRegex;
-//             errorMessage = emailErrorMsg;
-//             break;
-//         default:
-//             finalRegex = noRegex;
-//             errorMessage = "";
-//             break;
-//       }
-      
-//       if (finalRegex.test(inputValue)) {
-//         setIsValid(true);
-//         setValue(inputValue);
-//       } else {
-//         setIsValid(false);
-//       }
-//     };
-  
-//     return (
-//       <div>
-//         <input
-//           type="text"
-//           value={value}
-//           onChange={handleChange}
-//           id={id}
-//           style={{ borderColor: isValid ? 'initial' : 'red' }}
-//           required = {isRequired ? 'required' : ''}
-//           name= {id}
-//         />
-//         {!isValid && (
-//           <p style={{ color: 'red', fontSize: '0.8rem', marginTop: '0.2rem' }}>
-//             {errorMessage}
-//           </p>
-//         )}
-//       </div>
-//     );
-//   };
-  
-//   export default TextBox;
-
 import React, { useState, useEffect } from 'react';
+import '@styles/Components/TextBox.css';
 
 function TextBox({ fieldName, value, onChange }) {
   const [inputValue, setInputValue] = useState('');
@@ -97,7 +22,7 @@ function TextBox({ fieldName, value, onChange }) {
   };
 
   return (
-    <div>
+    <div className='input-container'>
       <label htmlFor={fieldName}>{fieldName}:</label>
       <input
         type="text"
@@ -106,7 +31,6 @@ function TextBox({ fieldName, value, onChange }) {
         value={value !== undefined ? value : inputValue}
         onChange={handleInputChange} 
       />
-      <p>Value entered for {fieldName}: {value !== undefined ? value : inputValue}</p>
     </div>
   );
 }
--- src/pages/Dashboard/Authentication/SignUp.jsx
+++ src/pages/Dashboard/Authentication/SignUp.jsx
@@ -3,35 +3,91 @@ import "@styles/Components/PageBody.css";
 import '@styles/Authentication/SignUp.css';
 import PageBody from "@components/Pages/PageBody";
 import Textbox from '@components/TextBox/TextBox.jsx';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const SignUp = () => {
 
     const imgBg = "/assets/images/sign-up.png";
+    const registerIC = "/assets/icons/register-icon.png";
+    const mobilenumberRegEx = 'const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/';
 
+    // obj to pass after submit
     const [inputValue, setInputValue] = useState({
-        AffiliateCode: '',
-        Username: '',
-        Password: '',
-        City: '',
-        MobileNumber: '',
-        FullName: '',
-        Email: '',
-        ConfirmPassword: ''
-        
+        affiliateCode: '',
+        username: '',
+        password: '',
+        city: '',
+        mobileNumber: '',
+        fullName: '',
+        email: '',
+        confirmPassword: '',
+        country: '',
+        nationality: '',
+        responsibilityCheckbox: false,
+        awareCheckbox: false
     });
 
-    const handleFirstName = (event) => {
+    const handleInputChange = (event) => {
         console.log(event);
-        const {name, value} = event.target;
+        const {name, value, type, checked} = event.target;
         
         setInputValue(prevValue => 
         ({
             ...prevValue,
-            [name]: value
+            [name]: type ==='checkbox' ? checked : value
         }));
 
         console.log(inputValue);
       };
+
+    const handleSubmit = (values) => {
+        console.log(values);
+        setFormValues(values);
+    }
+
+    // schema for validation
+    const SignupSchema = Yup.object().shape({
+        affiliateCode: Yup.string()
+            .min(2, 'Affiliate Code is too Short!')
+            .max(50, 'Affiliate Code is too Long!')
+            .required('Affiliate Code is Required'),
+        Username: Yup.string()
+            .min(2, 'Username is too Short!')
+            .max(20, 'Username is too Long!')
+            .required('Username is Required'),
+        Password: Yup.string()
+            .min(2, 'Password is too short!')
+            .max(20, 'Password is too Long!')
+            .required('Password is Required'),
+        City: Yup.string()
+            .required('City is Required'),
+        MobileNumber: Yup.string()
+            .matches(mobilenumberRegEx, 'Phone number is not valid')
+            .required('Mobile Number is Required'),
+        FullName: Yup.string()
+            .min(2, 'FullName is too Short!')
+            .max(100, 'FullName is too Long!')
+            .required('FullName Code is Required'),
+        Email: Yup.string().email('Invalid email').required('Required'),
+        ConfirmPassword: Yup.string()
+            .oneOf([Yup.ref("Password"), null], "Password must match")
+            .required('Field cannot be empty!'),
+        Country: Yup.string()
+            .min(2, 'Country is too Short!')
+            .max(100, 'Country is too Long!')
+            .required('Country is Required'),
+        Nationality: Yup.string()
+            .min(2, 'Nationality is too Short!')
+            .max(100, 'Nationality is too Long!')
+            .required('Nationality is Required'),
+        ResponsibilityCheckbox: Yup.boolean()
+            .oneOf([true], 'You must accept the terms and conditions')
+            .required('Required'),
+        AwareCheckbox: Yup.boolean()
+            .oneOf([true], 'You must accept the terms and conditions')
+            .required('Required')
+    });
 
     return (
         <>
@@ -56,104 +112,116 @@ const SignUp = () => {
 
                             <h1>Create New Account</h1>
 
-                            <div className="create-account-container" >
+                            <Formik
+                                initialValues={inputValue}
+                                validationSchema={SignupSchema}
+                                onSubmit={handleSubmit}
+                            >
+                            {({ errors, touched }) => (
+                                    <div>
+                                    <div className="create-account-container content-spacing" >
+                                        
+                                        <div className="create-account-content-left">
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.affiliateCode} fieldName='affiliateCode' onChange={handleInputChange} />
+                                                <ErrorMessage name="Affiliate Code" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.Username} fieldName='Username' onChange={handleInputChange} />
+                                                <ErrorMessage name="Username" component="div" />
+                                            </div>
+                                            
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.Password} fieldName='Password' onChange={handleInputChange} />
+                                                <ErrorMessage name="Password" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.City} fieldName='City' onChange={handleInputChange} />
+                                                <ErrorMessage name="City" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.MobileNumber} fieldName='Mobile Number' onChange={handleInputChange} />
+                                                <ErrorMessage name="Mobile Number" component="div" />
+                                            </div>
+                                            
+                                        </div>
+
+                                        <div className='flex-placeholder'></div>
+
+                                        <div className="create-account-content-left">
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.FullName} fieldName='Full Name' onChange={handleInputChange} />
+                                                <ErrorMessage name="Full Name" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.Email} fieldName='Email' onChange={handleInputChange} />
+                                                <ErrorMessage name="Email" component="div" />
+                                            </div>
+                                            
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.ConfirmPassword} fieldName='Confirm Password' onChange={handleInputChange} />
+                                                <ErrorMessage name="Confirm Password" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.Country} fieldName='Country' onChange={handleInputChange} />
+                                                <ErrorMessage name="Country" component="div" />
+                                            </div>
+
+                                            <div className="content-spacing">
+                                                <Textbox value={inputValue.Nationality} fieldName='Nationality' onChange={handleInputChange} />
+                                                <ErrorMessage name="Nationality" component="div" />
+                                            </div>
+                                            
+                                        </div>
+
+                                    </div>
+
+                                    <label>
+                                        <p>
+                                            <input 
+                                                type="checkbox" 
+                                                name={inputValue.ResponsibilityCheckbox} 
+                                                checked ={inputValue.ResponsibilityCheckbox} 
+                                                onChange={handleInputChange}
+                                            />
+                                            I understand and accept that as an Corsa Future Intervest Group Ltd. customer, it is my responsibility
+                                            to review all Terms of Business, Risk Disclosure, and Order Execution Policy set forth, and they can
+                                            be amended from time to time without prior notice.
+                                        </p>
+                                    </label>
+
+                                    <label>
+                                        <p>
+                                            <input 
+                                                type="checkbox" 
+                                                name={inputValue.ResponsibilityCheckbox} 
+                                                checked ={inputValue.ResponsibilityCheckbox} 
+                                                onChange={handleInputChange}
+                                            />
+                                            I understand and accept that when opening an Corsa Future Intervest Group Ltd. Account I need to
+                                            be aware of and abide by the laws of my local country to my best endeavours.
+                                        </p>
+                                    </label>
+
+                                    <div className="content-spacing">
+                                        <button className="register-button" type="submit">
+                                            <img src={registerIC}/>Register
+                                        </button>
+                                    </div>
+                                    
+
+                                    <p>By continuing, you agree to Privacy Policy and Terms of Service</p>
+                                </div>
                                 
-                                <div className="create-account-content-left">
-
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Affiliate Code
-                                        </p>
-                                        <Textbox value={inputValue.firstName} fieldName='firstName' onChange={handleFirstName} />
-                                    </div>
-
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Affiliate Code
-                                        </p>
-                                        <Textbox value={inputValue.lastname} fieldName='lastname' onChange={handleFirstName} />
-                                    </div>
-                                    
-                                    
-                                    {/* <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Username
-                                        </p>
-                                        <Textbox  fieldName='username'/>
-                                    </div> */}
-                                    
-                                    {/* <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Password
-                                        </p>
-                                        <Textbox  id='password'  regexType='5' isRequired='true'/>
-                                    </div>
-                                    
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            City
-                                        </p>
-                                        <Textbox  id='city' regexType='2' isRequired='true'/>
-                                    </div>
-                                    
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Mobile Number
-                                        </p>
-                                        <Textbox  id='mobile-number' regexType='3' isRequired='true'/>
-                                    </div>
-                                </div>
-
-                                <div className="create-account-content-left">
-
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Full Name
-                                        </p>
-                                        <Textbox  id='full-name' regexType='2' isRequired='true'/>
-                                    </div>
-
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Email 
-                                        </p>
-                                        <Textbox  id='email' regexType='4' isRequired='true'/>
-                                    </div>
-                                    
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Confirm Password
-                                        </p>
-                                        <Textbox  id='confirm-password' regexType='5' isRequired='true'/>
-                                    </div>
-                                    
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Country
-                                        </p>
-                                        <Textbox  id='country' regexType='2' isRequired='true'/>
-                                    </div>
-                                    
-                                    <div className="textbox-spacing">
-                                        <p> 
-                                            <span className="required-marker">*</span>
-                                            Nationality
-                                        </p>
-                                        <Textbox  id='nationality' regexType='2' isRequired='true'/>
-                                    </div> */}
-                                </div>
-                            </div>
-
+                            )}
+                            </Formik>
                             
                         </div>
                     </div>
