dc63563c566c6ab93f7785731e804553774e9884
letternine24 <cj.natividad24@gmail.com>
2024-09-20 16:36:05 +0800

    Get country

---
+++ src/components/TextBox/CountryTextbox.tsx
@@ -0,0 +1,42 @@
+import React, { ChangeEvent } from "react";
+import { useFetchCountries } from "@composables/cache/useFetchCountries";
+import { ICountry } from "@interface/cache/ICountry";
+
+interface CountrySelectionFormProps {
+  selectedCountry: string;
+  onCountrySelect: (selectedCountry: string) => void;
+}
+
+const CountrySelectionForm: React.FC<CountrySelectionFormProps> = ({ selectedCountry, onCountrySelect }) => {
+  const { countries, loading, error } = useFetchCountries();
+
+  const handleCountrySelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = e.target.value;
+    onCountrySelect(selectedValue);
+  };
+
+  return (
+    <div className="textbox-spacing">
+      {loading && <p>Loading countries...</p>}
+      {error && <p>Error loading countries: {error}</p>}
+
+      {!loading && countries && (
+        <select
+          id="country-dropdown"
+          value={selectedCountry}
+          onChange={handleCountrySelect}
+          required
+        >
+          <option value="">Select a country</option>
+          {countries.map((country: ICountry) => (
+            <option key={country.countryId} value={country.countryName}>
+              {country.countryName}
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+};
+
+export default CountrySelectionForm;
